69aa0e00b01b27d9e79299150b50c6ac1993dec0
Marie <marie.vaba@gmail.com>
2021-12-08 11:35:01 +0200

    added addBook functional by LS and ui

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const ls = new LS();
 const form = document.querySelector('#book-form');
 form.addEventListener('submit', addBook);
 
+document.addEventListener('DOMContentLoaded', getBooks);
+
+function getBooks() {
+    const books = ls.getData('books');
+    books.forEach(function (booksFromLS) {
+        ui.addBook(booksFromLS);
+    })
+}
+
 function addBook(event) {
 
     const title = ui.title.value;
@@ -11,6 +20,7 @@ function addBook(event) {
     const isbn = ui.isbn.value;
 
     const book = new Book(title, author, isbn);
+    console.log(book);
 
     ui.addBook(book);
 
