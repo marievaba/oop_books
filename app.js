cc2338938e4c9623e4232058e8e4facd892589a0
Marie <marie.vaba@gmail.com>
2021-12-10 11:05:14 +0200

    added filterBook functional by ui

--- app.js
+++ app.js
@@ -6,38 +6,43 @@ form.addEventListener('submit', addBook);
 
 document.addEventListener('DOMContentLoaded', getBooks);
 
-bookList = document.querySelector('#book-list');
+const bookList = document.querySelector('#book-list');
 bookList.addEventListener('click', delBook);
 
+const filter = document.querySelector("#filter");
+filter.addEventListener('keyup', filterBook);
+
+function filterBook(event) {
+    let filter = event.target.value.toLowerCase();
+    ui.filterData(filter);
+}
+
 function delBook(event) {
-    if (event.target.textContent === 'X'){
+    if (event.target.textContent === 'X') {
         const book = ui.getBook(event.target);
-        if (ui.delBook(event.target) === true){
+        if(ui.delBook(event.target) === true) {
             ls.delBook(book);
         }
     }
 }
 
+
+
 function getBooks() {
     const books = ls.getData('books');
-    books.forEach(function (booksFromLS ){
+    books.forEach(function (booksFromLS) {
         ui.addBook(booksFromLS);
     })
 }
 
 function addBook(event) {
-
     const title = ui.title.value;
     const author = ui.author.value;
     const isbn = ui.isbn.value;
-
     const book = new Book(title, author, isbn);
     console.log(book);
-
     ui.addBook(book);
-
     ls.addBook(book);
-
     ui.title.value = ''
     ui.author.value = ''
     ui.isbn.value = ''
--- classes/UI.js
+++ classes/UI.js
@@ -7,25 +7,39 @@ class UI {
     }
     addBook(book) {
         const tr = document.createElement('tr');
+        tr.setAttribute('class', 'book-row');
         tr.innerHTML = `<td>${book.title}</td>
                 <td>${book.author}</td>
                 <td>${book.isbn}</td>
                 <td><a href="#">X</a></td>`
         this.bookList.appendChild(tr);
     }
+
     getBook(click) {
-        let isbn = click.target.parentElement.previousElementSibling.textContent;
-        let author = click.target.parentElement.previousElementSibling.textContent;
-        let title = click.target.parentElement.previousElementSibling.textContent;
+        let isbn = click.parentElement.previousElementSibling.textContent;
+        let author = click.parentElement.previousElementSibling.previousElementSibling.textContent;
+        let title = click.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
         const book = new Book(title, author, isbn);
         return book;
     }
+
     delBook(click) {
-        if (confirm('Do you really want to delete this books?')) {
+        if (confirm('Do you realy want to delete this book?')) {
             click.parentElement.parentElement.remove();
-            return true
+            return true;
         } else {
-            return false
+            return false;
         }
+    }
+
+    filterData(filter) {
+        let bookListData = document.querySelectorAll('.book-row');
+        bookListData.forEach(function (trContent) {
+            if(trContent.textContent.toLowerCase().indexOf(filter) !== -1) {
+                trContent.style.display = '';
+            } else {
+                trContent.style.display = 'none';
+            }
+        })
     }
 }
